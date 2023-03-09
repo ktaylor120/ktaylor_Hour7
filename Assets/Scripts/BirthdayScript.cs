f5bc05d5679fefb0afef08a363ff99be97f9576a
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-03-08 23:16:02 -0800

    Birthday Script

--- Assets/Scripts/BirthdayScript.cs
+++ Assets/Scripts/BirthdayScript.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EvenNumberOutput : MonoBehaviour
+public class BirthdayScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 22; i < 102; i += 2)
-            Debug.Log(i);
+        int count = 0;
+        while (count <= 12)
+        {
+            Debug.Log(count);
+            count++;
+        }
     }
 
     // Update is called once per frame
