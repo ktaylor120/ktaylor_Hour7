931d7c366e38b973ea87ab321abee40798dc96b3
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-03-08 23:03:02 -0800

    Even Number Output loop

--- Assets/Scripts/EvenNumberOutput.cs
+++ Assets/Scripts/EvenNumberOutput.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImportantFunctions : MonoBehaviour
+public class EvenNumberOutput : MonoBehaviour
 {
-    public int runSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start runs before an object Updates");  
+        for (int i = 22; i < 102; i += 2)
+            print(i);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log ("This is called once a frame");
+        
     }
 }
